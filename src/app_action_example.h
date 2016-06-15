51fe41398099cdfb0c9bfdf0ce8403c969102ba7
Jon Barlow <orviwan@users.noreply.github.com>
2016-06-15 10:13:56 -0700

    Update app_action_example.h

--- src/app_action_example.h
+++ src/app_action_example.h
@@ -10,6 +10,7 @@ const char * prv_lockitron_status_message(LockitronLockState *state);
 static void prv_update_app_glance(AppGlanceReloadSession *session, size_t limit, void *context);
 static void prv_lockitron_toggle_state();
 static void prv_inbox_received_handler(DictionaryIterator *iter, void *context);
+static void prv_exit_delay();
 static void prv_exit_application(void *data);
 static void prv_init_app_message();
 static void prv_init(void);
